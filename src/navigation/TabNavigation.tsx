bf6c5fa3a9a8b43e832e06def051abddad339eda
Pabin R Luitel <pabin2050@gmail.com>
2022-01-21 11:58:35 +0545

    Add tab navigation icons

--- src/navigation/TabNavigation.tsx
+++ src/navigation/TabNavigation.tsx
@@ -1,16 +1,35 @@
 import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
 
 import FavouriteNotesScreen from '../screens/FavouriteNotes';
 import ArchivedNotesScreen from '../screens/ArchivedNotes';
 import NoteListScreen from '../screens/NoteList';
 import { getText } from '../assets/i18n';
+import { colors } from '../styles/colors';
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === 'Notes') {
+            iconName = 'home';
+          } else if (route.name === 'Favourite') {
+            iconName = 'heart';
+          } else {
+            iconName = 'archive';
+          }
+
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: colors.lightBlue900,
+        tabBarInactiveTintColor: 'gray',
+      })}>
       <Tab.Screen
         name="Notes"
         options={{ title: getText('tabs.notes') }}
